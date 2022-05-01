d44cd9b19b49b1bbb68a28ec5f945cf3d85e3218
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-05-01 22:10:02 +0600

    Responsive part

--- src/Pages/InventoryDetail/InventoryDetail.js
+++ src/Pages/InventoryDetail/InventoryDetail.js
@@ -9,10 +9,10 @@ const InventoryDetail = () => {
     const [quantity, setQuantity] = useState(location?.state?.quantity);
     console.log(location);
     return (
-        <div className='row'>
-            <div className='container mx-auto w-50'>
-                <div className='col-lg-9 col-md-6 col-12'>
-                    <div className='inventory-detail'>
+        <div>
+            <div className=' mx-auto w-50'>
+                <div className='container row row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-1'>
+                    <div className='inventory-detail col'>
                         <img className='img-fluid' src={location?.state?.img} alt="" />
                         <h2 className='inventory-name fst-italic fs-2'>Id: {id}</h2>
                         <h2 className='inventory-name fst-italic fs-4'>Name: {location?.state?.name}</h2>
@@ -23,12 +23,12 @@ const InventoryDetail = () => {
                         <h4>Quantity: {quantity}</h4>
                         <button className='btn-inventory btn btn-outline-light' onClick={() => setQuantity(quantity - 1)}>Delivered</button>
                     </div>
-                    <div className='col-lg-1 col-md-6 col-12'>
+                    <div className='mx-auto w-50 ms-4'>
                         <div>
-                            <div>
+                            <div className='mt-2'>
                                 <h5 className='inventory-name fst-italic fs-4'>Restock the items</h5>
                                 <input type="number" name="number" id="" />
-                                <button className='text-center mt-2 btn-inventory btn btn-outline-light' onClick={() => setQuantity(quantity + 1)}>Restock</button>
+                                <button className='text-center btn-inventory btn btn-outline-light' onClick={() => setQuantity(quantity + 1)}>Restock</button>
                             </div>
                         </div>
                     </div>
