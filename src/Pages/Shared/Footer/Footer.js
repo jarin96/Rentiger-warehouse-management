c3f23f50be0a314e74c96d8d82acea1d84f75f65
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-05-06 15:27:27 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='text-center'>
-            <p><small>copyright &copy; {new Date().getFullYear()}</small></p>
+        <footer className='py-5 bg-dark footer text-center'>
+            <div className='container-fluid'>
+                <p className='mt-5 text-center text-white'><small>copyright &copy; {new Date().getFullYear()}</small></p>
+                <h4 className='text-center text-white'>All rights reserved by Rentiger Warehouse</h4>
+            </div>
         </footer>
     );
 };
